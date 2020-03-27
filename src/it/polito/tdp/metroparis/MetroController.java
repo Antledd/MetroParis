e9f2147a3a890106a14f8b5617566ad66434387a
Antledd <alantled3@gmail.com>
2020-03-27 13:02:45 +0100

    Aggiustamenti su Metro.fxml e MetroController 26-5-2020)

--- src/it/polito/tdp/metroparis/MetroController.java
+++ src/it/polito/tdp/metroparis/MetroController.java
@@ -1,5 +1,33 @@
 package it.polito.tdp.metroparis;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
 public class MetroController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnCerca;
+
+
+    @FXML
+    void doCerca(ActionEvent event) {
+    }
+
+    @FXML
+    void initialize() {
+        assert btnCerca != null : "fx:id=\"btnCerca\" was not injected: check your FXML file 'Metro.fxml'.";
+
+
+    }
+
 }
